815a2388ed892c10e61a4eb9ffb1e05037f7260b
Yusuf Boyacigil <yboyacigil@gmail.com>
2013-07-10 18:14:40 +0000

    Added downloading from url

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLFILE_DOWNLOADED = "downloaded.html"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +68,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    var checkJson = null;
+    if (program.url) {
+	rest.get(program.url, {}).on('complete', 
+				 function (res) {
+				     if (res instanceof Error) {
+					 console.log('Error in downloading file: %s', res.message);
+				     } else {
+					 fs.writeFileSync(HTMLFILE_DOWNLOADED, res);
+				     }
+				 }
+				);
+	assertFileExists(HTMLFILE_DOWNLOADED);
+	checkJson = checkHtmlFile(HTMLFILE_DOWNLOADED, program.checks);
+    } else {
+	checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
